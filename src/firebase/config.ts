037b75015d472485d3a23e497dffa98620078463
Michelle Morais <michellepleite@gmail.com>
2024-04-02 23:44:20 -0300

    commit 009

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -10,6 +10,16 @@ if (!firebase.apps.length) {
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
       
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+
+        databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASER_URL,
+
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
+
     })
 }
 
